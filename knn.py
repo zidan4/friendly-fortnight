1f2a1f7192692bc8370404264e56b4269d59c914
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2025-01-25 15:08:59 +0300

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,1 +1,17 @@
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
 
+data = list(zip(x, y))
+knn = KNeighborsClassifier(n_neighbors=1)
+
+knn.fit(data, classes) 
+
+new_x = 8
+new_y = 21
+new_point = [(new_x, new_y)]
+
+prediction = knn.predict(new_point)
+
+plt.scatter(x + [new_x], y + [new_y], c=classes + [prediction[0]])
+plt.text(x=new_x-1.7, y=new_y-0.7, s=f"new point, class: {prediction[0]}")
+plt.show()
